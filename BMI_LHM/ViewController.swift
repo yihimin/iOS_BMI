1403943b224dde8cafb41974fc4b762f82eefed2
yihimin <yihimin01@gmail.com>
2024-11-07 16:25:55 +0900

    Initial Commit

---
+++ BMI_LHM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI_LHM
+//
+//  Created by Dahyun on 11/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
