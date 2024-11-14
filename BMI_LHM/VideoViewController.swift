c58ee2f448fbfd56bf9178dbd27d7e1af2221d13
yihimin <yihimin01@gmail.com>
2024-11-14 16:48:44 +0900

    비디오

---
+++ BMI_LHM/VideoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VideoViewController.swift
+//  BMI_LHM
+//
+//  Created by Dahyun on 11/14/24.
+//
+
+import UIKit
+import AVKit
+
+class VideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
