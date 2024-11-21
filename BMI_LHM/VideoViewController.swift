14fadde440648384ad87c4d9b160562eb5ce9fb3
yihimin <yihimin01@gmail.com>
2024-11-21 15:54:10 +0900

    video

--- BMI_LHM/VideoViewController.swift
+++ BMI_LHM/VideoViewController.swift
@@ -1,15 +1,19 @@
-//
-//  VideoViewController.swift
-//  BMI_LHM
-//
-//  Created by Dahyun on 11/14/24.
-//
-
 import UIKit
 import AVKit
 
 class VideoViewController: UIViewController {
 
+    @IBAction func playVideo(_ sender: UIButton) {
+        let videoPath : String? = Bundle.main.path(forResource: "bmi" , ofType: "mp4")
+        let videoURL = URL(filePath: videoPath!)
+        let player = AVPlayer(url: videoURL)
+        
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        present(playerController, animated: true)
+        player.play()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +21,6 @@ class VideoViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
